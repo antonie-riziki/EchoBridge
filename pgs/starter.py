6b96efd64e3579f07830049b08abd6f6ada2f75c
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-07-24 15:08:24 +0000

    updated the starter.py file

--- pgs/starter.py
+++ pgs/starter.py
@@ -9,8 +9,22 @@ st.markdown("""
     To begin developing with EchoBridge, you'll first need valid Africa’s Talking credentials.  
     Follow this guided process to set up your account and obtain API access.
     """)
-st.image('src/img/')
+st.image('src/img/Screen Shot 2025-07-24 at 17.20.46.png')
 
 st.info(f"""
-        Head to [Africas Talking]({at_url}) and create an account
+        **Note:** Visit [africastalking.com]({at_url}) and create an account
         """)
+
+st.write("""
+    after clicking on login, a new window will load for user autherntication processes
+    \n- You can either create an account (if you dont have) or 
+    \n- Enter your login credentials (email and password)
+
+        """)
+
+st.image('src/img/Screen Shot 2025-07-24 at 17.16.18.png')
+st.success("""
+
+    **Tip:** You can login using either Github, google or Microsoft account
+
+    """)
