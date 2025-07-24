8275503ba940c5d474d7b7561dfc42925bdd6b0b
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-07-24 14:38:37 +0000

    updated the starter.py file, added src file

--- pgs/starter.py
+++ pgs/starter.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
-
+at_url = "https://africastalking.com/"
 st.header("Getting Started")
 
 st.subheader("Create an account")
+
 st.markdown("""
     To begin developing with EchoBridge, you'll first need valid Africa’s Talking credentials.  
     Follow this guided process to set up your account and obtain API access.
     """)
-st.markdown("""
-        Head to Africas Talking [https://africastalking.com/] and create an account
+# st.image('src/img/')
+
+st.info(f"""
+        Head to [Africas Talking]({at_url}) and create an account
         """)
