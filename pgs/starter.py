928522fb637e33a6b6dafcfd40f9313b92f4cdc9
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-07-24 14:28:48 +0000

    updated the starter.py file

---
+++ pgs/starter.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.header("Getting Started")
+
+st.subheader("Create an account")
+st.markdown("""
+    To begin developing with EchoBridge, you'll first need valid Africa’s Talking credentials.  
+    Follow this guided process to set up your account and obtain API access.
+    """)
+st.markdown("""
+        Head to Africas Talking [https://africastalking.com/] and create an account
+        """)
