d2abed25d58d8e27d7c11dcdaa86840c22acd806
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-07-25 15:58:37 +0000

    updated the sms.py file

--- pgs/sms_service.py
+++ pgs/sms_service.py
@@ -1,5 +1,20 @@
 import streamlit as st 
 
+from code_editor import code_editor
+
+
+st.markdown(
+    """
+    <div class=title>
+        <div style=" justify-content: center;">
+            <h3 style="text-align: center; margin-top: -50px; color: #007B8A;">Messaging</h3>
+            <p style="text-align: center;">Your API Sandbox and Orchestration Hub.</p>
+        </div>
+    </div> 
+    """,
+    unsafe_allow_html=True,
+)
+
 st.image('https://miro.medium.com/v2/resize:fit:1000/1*Ulfk0ZmOY71uF9_0aCGmvw.png')
 
 st.info("""
@@ -34,3 +49,65 @@ st.info("""
     This model enables monetized service delivery without requiring external payment gateways.  
 
 """)
+
+st.subheader("Tutorial & Demo")
+
+sample_sms_code = """
+    import africastalking
+    import otp_sms
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    africastalking.initialize(
+        username='EMID',
+        api_key = os.getenv("AT_API_KEY")
+    )
+
+
+    sms = africastalking.SMS
+
+    def send_message():
+        recipients = ["+2547xxxxxxxx"]
+
+        print(recipients)
+        print(phone_number)
+
+        # Set your message
+        message = "Hello World!";
+
+        # Set your shortCode or senderId
+        sender = 20880
+
+        try:
+            
+            response = sms.send(message, recipients, sender)
+
+            print(response)
+
+        except Exception as e:
+            print(f'Houston, we have a problem: {e}')
+
+"""
+
+languages = ["Python", "Node.js", "Javascript", "Ruby", "Typescript", "Rust", "Kotlin", "Php", "Java"]
+themes = ["default", "light", "dark", "contrast"]
+
+with st.form('Settings'):
+    col1, col2 = st.columns(2)
+
+    with col1:
+        language = st.selectbox(label="Select language", options=sorted(languages, reverse=False))
+    
+    with col2:
+        theme = st.selectbox(label="theme", options=themes)
+
+    submit = st.form_submit_button("Submit", type="primary", use_container_width=True)
+
+
+if submit:
+    if language=="Python":
+        code = code_editor(sample_sms_code)
+
+    else:
+        pass
