5b55b911583048cef055d50bf644090f0378e7ec
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-07-25 10:51:07 +0000

    updated the starte file

--- pgs/starter.py
+++ pgs/starter.py
@@ -36,14 +36,52 @@ st.write("""
        \n- Go to AT Sandbox
 """)
 
-st.image()
+st.image('src/img/Screen Shot 2025-07-24 at 21.22.44.png')
 
 st.info("""
 
     **Note:** if you have just registered your account, you dont have team yet, so we have to create one
 
 """)
+st.subheader('Create a Team')
+st.markdown("""
+    Click on **New Team** to create a new team
+    \nEnter your preffered Team Name, name it something cool 
+""")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.image('src/img/Screen Shot 2025-07-24 at 21.23.01.png')
+
+with col2:
+    st.image('src/img/Screen Shot 2025-07-25 at 13.05.16.png')
+
+st.success("""
+
+    **Tip:** Team name **MUST** be a Unique Generic name
+
+""")
+
+st.subheader('Create Application + Add Members')
 
 st.markdown("""
-    
+
+    Once the team is created, we can now proceed to create our application and add 
+    memebers to the application and manage other settings of the team. 
+
 """)
+
+st.info("""
+
+    **Note:** As an app owner you can restrict the access levels of your team memebers
+    \n as well as data visibility of the application.
+
+""")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('src/img/Screen Shot 2025-07-25 at 13.06.13.png')
+
+with col2: 
+    st.image('src/img/Screen Shot 2025-07-25 at 13.06.29.png')
