befba4304ffb9a2c987d1731664b54f460118aa0
Decimal Hardware <decimalhardware.ke@gmail.com>
2025-07-28 13:10:09 +0000

    updated the code generation function

--- pgs/airtime.py
+++ pgs/airtime.py
@@ -37,3 +37,69 @@ Create systems where agents or resellers distribute airtime at scale, benefiting
 **Corporate Allowance Programs**  
 Disburse airtime allowances to staff or field agents directly through your application, automating routine top‑ups and tracking usage centrally for transparency.
 """)
+
+
+st.subheader("Tutorial & Demo")
+
+sample_airtime_code = """
+    import africastalking
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    africastalking.initialize(
+        username='USERNAME',
+        api_key = os.getenv("AT_API_KEY")
+    )
+
+
+    airtime = africastalking.Airtime
+
+    def send_message():
+        amount="10"
+        currency_code = "KES"
+        
+        recipients = "+254" + str(phone_number)
+
+        print(recipients)
+        print(phone_number)
+
+        try:
+            
+            responses = airtime.send(phone_number=recipients, amount=amount, currency_code=currency_code)
+
+            print(response)
+
+        except Exception as e:
+            print(f'Houston, we have a problem: {e}')
+
+"""
+
+languages = ["Python", "Node.js", "Javascript", "Ruby", "Typescript", "Rust", "Kotlin", "Php", "Java"]
+themes = ["default", "light", "dark", "contrast"]
+
+with st.form('Settings'):
+    col1, col2 = st.columns(2)
+
+    with col1:
+        language = st.selectbox(label="Select language", options=sorted(languages, reverse=False))
+    
+    with col2:
+        theme = st.selectbox(label="theme", options=themes)
+
+    submit = st.form_submit_button("Submit", type="primary", use_container_width=True)
+
+
+if submit:
+    if language=="Python":
+        code = code_editor(sample_airtime_code, theme=theme, allow_reset=True, lang='python')
+
+    else:
+        
+        st.warning("""
+            **⚠️ Warning:**  
+            This code has been automatically generated. Please **review thoroughly, test extensively**, and validate every assumption or edge case before deploying. Do not rely solely on its correctness or security.
+        """)
+        code_editor(autogenerate_code_samples(sample_airtime_code, language), theme=theme, allow_reset=True, lang=language.lower())
+  
